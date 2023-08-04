30ebb774349ef1d3861ff4c63964cd82cc1240e2
Vraj Raval <93474512+Vraj-Raval@users.noreply.github.com>
2023-08-04 21:16:03 +0530

    Update 196-Delete Dublicate Emails.sql

--- SQL/196-Delete Dublicate Emails.sql
+++ SQL/196-Delete Dublicate Emails.sql
@@ -1,3 +1,5 @@
+/**
+Table:Person
 +-------------+---------+
 | Column Name | Type    |
 +-------------+---------+
@@ -40,7 +42,8 @@ Output:
 +----+------------------+
 Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
 -----------------------------------------------------------------------------------------------------------  
-# Write your MySQL query statement below
+**/
+
 DELETE p1
 FROM Person p1 , Person p2 
 WHERE p1.email=p2.email AND p1.id>p2.id
