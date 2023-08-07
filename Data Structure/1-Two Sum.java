5140230d88412f83873e6f43ca16f6ca57045be9
Vraj Raval <93474512+Vraj-Raval@users.noreply.github.com>
2023-08-07 23:23:21 +0530

    Update and rename 1-Two Sum.cs to 1-Two Sum.java

--- Data Structure/1-Two Sum.java
+++ Data Structure/1-Two Sum.java
@@ -21,3 +21,19 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      int n = nums.length;
+      for(int i=0;i<n-1;i++)
+      {
+          for(int j=i+1;j<n;j++)
+          {
+              if(nums[i]+nums[j]==target)
+              {
+                  return new int[]{i,j};
+              }
+          }
+      }  
+      return new int[]{};
+    }
+}
